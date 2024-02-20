39ba9637f57588f75f4fd043b3fbedb1c0006a2f
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-02-20 11:44:25 -0300

    Agrego entity Product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductsModule } from './products/products.module';
 import * as process from "process";
 
 @Module({
@@ -16,7 +17,9 @@ import * as process from "process";
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true, //para sincronizar las entities con las columnas al modificar
-    })
+    }),
+
+    ProductsModule
   ],
 })
 export class AppModule {}
