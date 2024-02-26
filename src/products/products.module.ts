c029b9cc5626071a6fc66826eb224d81c619712c
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-02-26 13:31:23 -0300

    agrego auth a products y seed

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,13 +3,15 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Product, ProductImage } from "./entities";
+import { AuthModule } from "../auth/auth.module";
 // import { ProductImage } from "./entities/product-image.entity"; //por archivo de barril index.ts
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
-    TypeOrmModule.forFeature([Product, ProductImage]) //arreglo con todas las entidades que está definiendo
+    TypeOrmModule.forFeature([Product, ProductImage]), //arreglo con todas las entidades que está definiendo
+    AuthModule
   ],
   exports: [ ProductsService ]
 })
