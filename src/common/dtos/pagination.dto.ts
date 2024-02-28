e6483c48903580a757c242d186c97c2bb0e5d691
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-02-27 23:08:49 -0300

    Agego ApiProperty a dto de querysParameters

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,25 @@
 import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
 import { Type } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class PaginationDto{
+
+  @ApiProperty({
+    default: 10,
+    description: 'Cuantas filas desea ver?',
+    required: false
+  })
   @IsOptional()
   @IsPositive()
   @Type(()=> Number) //evisamos agregar el transformer el app.module
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'Cuantas filas desea saltar?',
+    required: false
+  })
   @IsOptional()
   @Type(()=> Number)
   @Min(1)
